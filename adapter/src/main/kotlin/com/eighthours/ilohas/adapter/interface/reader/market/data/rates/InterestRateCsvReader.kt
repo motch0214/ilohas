e704dd42b9e6c99794a16bf112e98c0809de8bbf
motch0214 <motch0214@gmail.com>
2018-05-06 16:22:02 +0900

    feat: create csv reader

---
+++ adapter/src/main/kotlin/com/eighthours/ilohas/adapter/interface/reader/market/data/rates/InterestRateCsvReader.kt
@@ -0,0 +1,21 @@
+package com.eighthours.ilohas.adapter.`interface`.reader.market.data.rates
+
+import com.eighthours.ilohas.adapter.util.CsvReader
+import com.eighthours.ilohas.app.`interface`.InterfaceReader
+import com.eighthours.ilohas.app.`interface`.market.data.rates.InterestRateDto
+import org.apache.commons.csv.CSVFormat
+import org.apache.commons.csv.CSVRecord
+import org.springframework.stereotype.Component
+
+
+@Component
+class InterestRateCsvReader : CsvReader<InterestRateDto>(), InterfaceReader<InterestRateDto> {
+
+    override val format: CSVFormat = CSVFormat.DEFAULT.withFirstRecordAsHeader()
+
+    override fun CSVRecord.parse() = InterestRateDto(
+            currency = this["Currency"],
+            indexName = this["IndexName"],
+            term = this["Term"],
+            rate = this["Rate"])
+}
---
+++ adapter/src/main/kotlin/com/eighthours/ilohas/adapter/util/csv.kt
@@ -0,0 +1,22 @@
+package com.eighthours.ilohas.adapter.util
+
+import org.apache.commons.csv.CSVFormat
+import org.apache.commons.csv.CSVParser
+import org.apache.commons.csv.CSVRecord
+import java.nio.file.Files
+import java.nio.file.Path
+
+
+abstract class CsvReader<out T> {
+
+    protected abstract val format: CSVFormat
+
+    fun read(file: Path, block: (Sequence<T>) -> Unit) {
+        return Files.newBufferedReader(file).use {
+            val seq = CSVParser(it, format).asSequence().map { it.parse() }
+            block(seq)
+        }
+    }
+
+    protected abstract fun CSVRecord.parse(): T
+}
