d15afc4ffa467c7aa80537b190d2d24adb164c87
motch0214 <motch0214@gmail.com>
2018-06-06 22:21:46 +0900

    feat: apply element-ui theme (#432073)

--- web/src/main.js
+++ web/src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue';
 import App from './App';
 import router from './router';
 import Element from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
+import './styles/theme/index.css';
 import 'normalize.css';
 
 Vue.use(Element);
